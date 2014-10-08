45ff1bc531ef2c658a9d4208973f40dcbc98f103
Let's Developer <me@letsdeveloper.com>
2014-10-08 23:30:14 +0200

    #1 - Problem Statement

---
+++ src/com/letsdeveloper/refactoring/movierental/Customer.java
@@ -0,0 +1,64 @@
+package com.letsdeveloper.refactoring.movierental;
+
+import java.util.List;
+
+public class Customer {
+	private String name;
+	private List<Rental> rentals;
+
+	public Customer(String name) {
+		this.name = name;
+		this.rentals = new java.util.ArrayList<Rental>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void addRental(Rental arg) {
+		rentals.add(arg);
+	}
+	
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		String result = "Rental Record for " + getName() + "\n";
+		for (Rental each : rentals) {
+			double thisAmount = 0;
+			
+			// determine amounts for each line
+			switch (each.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2) {
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				}
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3){
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				}
+				break;
+			}
+			
+			// add frequent renter points
+			frequentRenterPoints++;
+			// add bonus for a two day new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+				frequentRenterPoints++;
+			}
+			
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+		// add footer lines
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		return result;
+	}
+}
---
+++ src/com/letsdeveloper/refactoring/movierental/Movie.java
@@ -0,0 +1,27 @@
+package com.letsdeveloper.refactoring.movierental;
+
+public class Movie {
+	public static final int CHILDRENS = 1;
+	public static final int REGULAR = 2;
+	public static final int NEW_RELEASE = 3;
+
+	private String title;
+	private int priceCode;
+
+	public Movie(String title, int priceCode) {
+		this.title = title;
+		this.priceCode = priceCode;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getPriceCode() {
+		return priceCode;
+	}
+
+	public void setPriceCode(int priceCode) {
+		this.priceCode = priceCode;
+	}
+}
