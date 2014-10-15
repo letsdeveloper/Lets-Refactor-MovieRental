9d67c782818fa4d35330a5064ce70b58676467bc
Let's Developer <me@letsdeveloper.com>
2014-10-16 00:13:54 +0200

    #5 - Move Methods

--- src/com/letsdeveloper/refactoring/movierental/Customer.java
+++ src/com/letsdeveloper/refactoring/movierental/Customer.java
@@ -25,45 +25,14 @@ public class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		for (Rental each : rentals) {
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(getAmountFor(each)) + "\n";
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
 			
-			totalAmount += getAmountFor(each);
-			totalFrequentRenterPoints += getFrequentRenterPointsFor(each);
+			totalAmount += each.getCharge();
+			totalFrequentRenterPoints += each.getFrequentRenterPoints();
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(totalFrequentRenterPoints) + " frequent renter points";
 		return result;
-	}
-
-	private int getFrequentRenterPointsFor(Rental rental) {
-		int thisFrequentRenterPoints = 1;
-		// add bonus for a two day new release rental
-		if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) {
-			thisFrequentRenterPoints++;
-		}
-		return thisFrequentRenterPoints;
-	}
-
-	private double getAmountFor(Rental rental) {
-		double thisAmount = 0;
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (rental.getDaysRented() > 2) {
-				thisAmount += (rental.getDaysRented() - 2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (rental.getDaysRented() > 3){
-				thisAmount += (rental.getDaysRented() - 3) * 1.5;
-			}
-			break;
-		}
-		return thisAmount;
 	}
 }
--- src/com/letsdeveloper/refactoring/movierental/Rental.java
+++ src/com/letsdeveloper/refactoring/movierental/Rental.java
@@ -16,4 +16,34 @@ public class Rental {
 	public Movie getMovie() {
 		return movie;
 	}
+
+	double getCharge() {
+		double result = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (getDaysRented() > 2) {
+				result += (getDaysRented() - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (getDaysRented() > 3){
+				result += (getDaysRented() - 3) * 1.5;
+			}
+			break;
+		}
+		return result;
+	}
+
+	int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+			return 2;
+		} else {
+			return 1;
+		}
+	}
 }
